52d9bb57af72a73ca6940851c828f0a90eef4f1c
tykhonova <liubov.tykhonova@globallogic.com>
2017-01-10 17:35:39 +0200

    added more views

--- posts/views.py
+++ posts/views.py
@@ -1,7 +1,44 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http  import HttpResponse
 
 # Create your views here.
+from .models import Post
 
-def post_home(request):
-    return HttpResponse("<h1>Hello</h1>")
+
+def post_create(request):
+    return HttpResponse("<h1>Create</h1>")
+
+def post_detail(request):
+    # instance = Post.objects.get(id=1)
+
+    instance = get_object_or_404(Post, id=1)
+    context = {
+        "title": instance.title,
+        "instance" : instance
+    }
+    return render(request, "post_detail.html", context)
+
+def post_list(request):
+    # if request.user.is_authenticated():
+    #     context = {
+    #         "title": " my user List"
+    #     }
+    # else:
+    #     context = {
+    #         "title": "List"
+    #     }
+    queryset = Post.objects.all()
+    context = {
+        "object_list" : queryset,
+
+        "title": "List"
+    }
+    return render(request, "index.html", context)
+
+def post_update(request):
+    return HttpResponse("<h1>Update</h1>")
+
+def post_delete(request):
+    return HttpResponse("<h1>Delete</h1>")
+
+
--- try_django/urls.py
+++ try_django/urls.py
@@ -13,12 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import include, url
 from django.contrib import admin
 
 from posts import views as post_view
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^posts/$', post_view.post_home),
+    url(r'^posts/', include("posts.urls")),
 ]
