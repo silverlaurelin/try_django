2fd5aeeccdc6a927ae3448f52d82dd53fcaf3b66
tykhonova <liubov.tykhonova@globallogic.com>
2017-01-13 18:08:55 +0200

    added  templates

--- posts/views.py
+++ posts/views.py
@@ -1,44 +1,70 @@
-from django.shortcuts import render, get_object_or_404
-from django.http  import HttpResponse
+from django.shortcuts import render, get_object_or_404, redirect
+from django.http  import HttpResponse, HttpResponseRedirect
+from django.contrib import messages
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.shortcuts import render
+from urllib import quote_plus
 
-# Create your views here.
 from .models import Post
+from .forms import PostForm
 
 
 def post_create(request):
-    return HttpResponse("<h1>Create</h1>")
+    form = PostForm(request.POST or None, request.FILES or None)
+    if form.is_valid():
+        instance = form.save(commit = False)
+        instance.save()
+        messages.success(request, "Successfully Created!")
+        return HttpResponseRedirect(instance.get_absolute_url())
+    context = {
+        "form": form,
+    }
+    return render(request, "post_form.html", context)
 
-def post_detail(request):
-    # instance = Post.objects.get(id=1)
-
-    instance = get_object_or_404(Post, id=1)
+def post_detail(request, id=None):
+    instance = get_object_or_404(Post, id=id)
+    share_string = quote_plus(instance.content)
     context = {
         "title": instance.title,
-        "instance" : instance
+        "instance" : instance,
+        "share_string" : share_string
     }
     return render(request, "post_detail.html", context)
 
 def post_list(request):
-    # if request.user.is_authenticated():
-    #     context = {
-    #         "title": " my user List"
-    #     }
-    # else:
-    #     context = {
-    #         "title": "List"
-    #     }
-    queryset = Post.objects.all()
+    queryset_list = Post.objects.all()
+    paginator = Paginator(queryset_list, 5) # Show 25 contacts per page
+
+    page = request.GET.get('page')
+    try:
+        queryset = paginator.page(page)
+    except PageNotAnInteger:
+        queryset = paginator.page(1)
+    except EmptyPage:
+        queryset = paginator.page(paginator.num_pages)
     context = {
         "object_list" : queryset,
-
         "title": "List"
     }
-    return render(request, "index.html", context)
+    return render(request, "post_list.html", context)
 
-def post_update(request):
-    return HttpResponse("<h1>Update</h1>")
+def post_update(request, id=None):
+    instance = get_object_or_404(Post, id=id)
+    form = PostForm(request.POST or None, request.FILES or None, instance=instance)
+    if form.is_valid():
+        instance = form.save(commit=False)
+        instance.save()
+        messages.success(request, "Updated!", extra_tags="some-tag")
+        return HttpResponseRedirect(instance.get_absolute_url())
+    context = {
+        "title": instance.title,
+        "instance": instance,
+        "form": form
+    }
+    return render(request, "post_form.html", context)
 
-def post_delete(request):
-    return HttpResponse("<h1>Delete</h1>")
-
-
+def post_delete(request, id=None):
+    instance = get_object_or_404(Post, id=id)
+    instance.delete()
+    messages.success(request, "Deleted!")
+    return redirect("posts:list")
--- try_django/urls.py
+++ try_django/urls.py
@@ -15,10 +15,16 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.conf.urls.static import static
+from django.conf import settings
 
 from posts import views as post_view
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^posts/', include("posts.urls")),
+    url(r'^posts/', include("posts.urls", namespace = "posts")),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
